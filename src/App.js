bc81fccdabbe4e91009cc97b5ce0332ebaafda76
Clay <noitseuq.rm@gmail.com>
2023-02-17 10:53:26 +0800

    lesson 32: Monsters Rolodex - setState and Secondary Callback

--- src/App.js
+++ src/App.js
@@ -20,8 +20,16 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>Hi { this.state.name.firstName } { this.state.name.lastName }, I work at { this.state.company }</p>
           <button onClick={() => {
-            this.setState({ name: { firstName: 'Andrei', lastName: 'Neaogie' }});
-            console.log(this.state);
+            this.setState(
+              () => {
+                return {
+                  name: { firstName: 'Andrei', lastName: 'Neaogie' }
+                }
+              },
+              () => {
+                console.log(this.state);
+              }
+            );
           }}>
             Change Name
           </button>
